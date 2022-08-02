598f4cad331f6596f5afa161536e64fe3eecedeb
Dilas Campos <86895535+DilasCampos@users.noreply.github.com>
2022-08-01 22:03:04 -0300

    first commit

---
+++ src/one/digitalinnovation/gof/singleton/SingletonLazy.java
@@ -0,0 +1,24 @@
+package one.digitalinnovation.gof.singleton;
+
+/**
+ * Singleton "pregui�oso".
+ * 
+ * @author falvojr
+ */
+public class SingletonLazy {
+
+	private static SingletonLazy instancia;
+	
+	private SingletonLazy() {
+		super();
+	}
+	
+	public static SingletonLazy getInstancia() {
+		if (instancia == null) {
+			instancia = new SingletonLazy();
+		}
+		return instancia;
+	}
+}
+
+
